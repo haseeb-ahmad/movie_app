6d232058898566644a4bd8b2dedb50080bc382e9
Haseeb Ahmad <engg.haseeb@gmail.com>
2025-01-07 17:25:41 +0500

    Email password not optional for user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
